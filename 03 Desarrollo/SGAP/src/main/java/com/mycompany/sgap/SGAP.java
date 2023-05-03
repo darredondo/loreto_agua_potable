5ec15ce825a57c808f9b268837ac3813d94f82b1
ImeldaChavez <imeldaguadalupe08@gmail.com>
2023-05-03 12:24:16 -0600

    Creamos proyecto en Java

---
+++ 03 Desarrollo/SGAP/src/main/java/com/mycompany/sgap/SGAP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sgap;
+
+/**
+ *
+ * @author Imelda Chavez
+ */
+public class SGAP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
