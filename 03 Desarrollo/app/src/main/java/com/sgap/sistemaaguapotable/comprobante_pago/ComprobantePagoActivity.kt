2cbceb1b2fb8fa9aaa01bf392e2254d7fa8a580b
Diego Alfredo Cardoza Ojeda <85135606+DiegoCardoza@users.noreply.github.com>
2023-05-03 13:32:23 -0600

    Se creo el proyecto

---
+++ 03 Desarrollo/app/src/main/java/com/sgap/sistemaaguapotable/comprobante_pago/ComprobantePagoActivity.kt
@@ -0,0 +1,12 @@
+package com.sgap.sistemaaguapotable.comprobante_pago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sgap.sistemaaguapotable.R
+
+class ComprobantePagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comprobante_pago)
+    }
+}
---
+++ 03 Desarrollo/app/src/main/java/com/sgap/sistemaaguapotable/pago/PagoActivity.kt
@@ -0,0 +1,12 @@
+package com.sgap.sistemaaguapotable.pago
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sgap.sistemaaguapotable.R
+
+class PagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pago)
+    }
+}
