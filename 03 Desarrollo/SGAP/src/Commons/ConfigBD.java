f0964bfd7d5c107a109f1e985c7f88c480470641
Alejandro3407 <125064627+Alejandro3407@users.noreply.github.com>
2023-06-06 08:56:12 -0600

    cree la interfaz

---
+++ 03 Desarrollo/SGAP/src/Commons/ConfigBD.java
@@ -0,0 +1,9 @@
+package Commons;
+
+public class ConfigBD {
+    static public String Servidor="localhost";
+    static public String BaseDatos="loreto_agua_potable";
+    static public String Usuario="admin";
+    static public String Contrasenna="1234";
+}
+
